7b8136690cab26a0098e3e14b6f4bf509a87a23a
hank9999 <hank9999@qq.com>
2020-08-14 09:31:02 +0800

    Remove redundant base64 encryption

--- Encryption.py
+++ Encryption.py
@@ -14,12 +14,10 @@ async def encrypt(data):
     aes = AES.new(add_to_16(EncryptionKey), AES.MODE_ECB)
     encrypt_aes = aes.encrypt((pad(data.encode('utf-8'), 16)))
     encrypted_text = str(base64.encodebytes(encrypt_aes), encoding='utf-8')
-    encrypted_text_base64 = base64.b64encode(encrypted_text.encode('utf-8')).decode('utf-8')
-    return encrypted_text_base64
+    return encrypted_text
 
 
 async def decrypt(data):
-    data = base64.b64decode(data).decode('utf-8')
     aes = AES.new(add_to_16(EncryptionKey), AES.MODE_ECB)
     base64_decrypted = base64.decodebytes(data.encode(encoding='utf-8'))
     decrypted_text = unpad(aes.decrypt(base64_decrypted), 16).decode('utf-8')
